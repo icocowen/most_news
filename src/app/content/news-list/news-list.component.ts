35bad6418c00cc3bdb1498417b106f5d36896bff
icocowen <1558165507@qq.com>
2019-04-08 23:09:59 +0800

    finish function2

--- src/app/content/news-list/news-list.component.ts
+++ src/app/content/news-list/news-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { differenceInHours, distanceInWords } from 'date-fns';
 import { NewsServerService } from 'src/app/data/news-server.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-news-list',
@@ -9,7 +11,7 @@ import { NewsServerService } from 'src/app/data/news-server.service';
 })
 export class NewsListComponent implements OnInit {
 
-  public data;
+  public data: any[];
 
   constructor(
     private newsServer: NewsServerService
@@ -17,6 +19,16 @@ export class NewsListComponent implements OnInit {
 
   ngOnInit() {
     this.newsServer.getData().subscribe(d => this.data = d);
+    fromEvent(window, 'scroll').pipe(debounceTime(400)).subscribe(_ => {
+      let scrollTop = document.documentElement.scrollTop;
+      let pageH = document.body.offsetHeight;
+      let clientH = document.body.clientHeight;
+      if(scrollTop + clientH + 100 > pageH) {
+        this.newsServer.getData().subscribe(d => {
+          this.data = this.data.concat(d);  
+        });
+      }
+    })
   }
 
 }
